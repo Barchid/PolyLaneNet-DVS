9c1d16bee2ddc4bf6b24afbd6291615f6ca4dd5b
Sami Barchid <sami.barchid@univ-lille.fr>
2023-02-02 14:34:50 +0100

    go

---
+++ cmds.sh
@@ -0,0 +1,5 @@
+python train.py --exp_name nopretrain_snn --cfg cfgs/detbinary_fulltrain_snn.yaml
+
+python train.py --exp_name nopretrain_cnn --cfg cfgs/detbinary_fulltrain_cnn.yaml
+
+python train.py --exp_name nopretrain_3dcnn --cfg cfgs/detbinary_fulltrain_3dcnn.yaml
